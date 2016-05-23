33addd29c221eef8ab167c2c176f8c57299dd1e3
Petro PIstun <petropistun@gmail.com>
2016-05-24 00:09:48 +0300

    First draft sources for esp

---
+++ src/awsservo/awsservoconfig.h
@@ -0,0 +1,8 @@
+//AWS IOT config, change these:
+char wifi_ssid[]       = "wifi";
+char wifi_password[]   = "wifipass";
+char aws_endpoint[]    = "EXAMPLE.iot.us-west-2.amazonaws.com";
+char aws_key[]         = "EXAMPLE_KEY";
+char aws_secret[]      = "EXAMPLE_KEY_SECRET";
+char aws_region[]      = "us-west-2";
+const char* aws_topic  = "$aws/things/esp-servo/shadow/update";
